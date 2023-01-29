39fbf00e37bd13c6f3cb7484d7b5081131b5e80e
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2023-01-29 18:14:45 +0600

    Company, Employee, Device, DeviceLog Model added

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Company, Employee, Device, DeviceLog
 # Register your models here.
+
+admin.site.register([Company, Employee, Device, DeviceLog])
+
--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from authentications.models import UserAccount as User
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    company_license = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    model = models.CharField(max_length=100, blank=True, null=True)
+    brand = models.CharField(max_length=100, blank=True, null=True)
+    serial_number = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class DeviceLog(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    checkout_condition = models.TextField(blank=True, null=True)
+    checkin_date = models.DateTimeField(blank=True, null=True)
+    checkin_condition = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
